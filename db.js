46928d3446a22879f5f76b9953b1ae4fbe69f2fb
tony2037 <ztex030640417@gmail.com>
2017-11-26 22:23:13 +0800

    db

--- db.js
+++ db.js
@@ -40,13 +40,3 @@ function Subscribe(User_ID= "",subscribe){
     else{}
 }
 
-=======
-mongoose.connect('mongodb://wp2017_groupk:uorgn68c@localhost/wp2017_groupk');
-var db_connection = mongoose.connection;
-    // if not connected
-db_connection.on('error', console.error.bind(console, 'connection error:'));
-db_connection.once('open', function() {
-    // if connected
-    var MyModel = mongoose.model('Test', new mongoose.Schema({ name: String }));
-});
->>>>>>> 0c0c48e46482a36240c3523d2487a4ed1dc45c27
