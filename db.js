777729c7454009ec8503b0b0a02810fe557be3ce
tony2037 <ztex030640417@gmail.com>
2017-11-26 21:11:07 +0800

    db.js

--- db.js
+++ db.js
@@ -1,4 +1,23 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://wp2017_groupk:uorgn68c@localhost/wp2017_groupk');
+var Admin = mongoose.mongo.Admin;
+const connection = mongoose.createConnection('mongodb://wp2017_groupk:uorgn68c@localhost/wp2017_groupk');
 
-var MyModel = mongoose.model('Test', new mongoose.Schema({ name: String }));
+var Schema = mongoose.Schema;
+var UsersSchema = new Schema({
+    id: String,
+    channel_free:{subscibe: Boolean,po:Number,},
+    channel_pay: [{name:String, po: Number, subscribe: Boolean}]
+});
+
+
+var Users = mongoose.model("Users",UsersSchema);
+
+connection.on('open', ()=>{
+    new Admin(connection.db).listDatabases((err, result)=>{
+        console.log('listDatabases succeeded');
+
+        console.log(result);
+    });
+});
+
+
